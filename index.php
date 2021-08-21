33df7cd78b830030979fdb070378df50f515bebd
vikrantup72 <37727808+vikrantup72@users.noreply.github.com>
2021-08-22 00:02:38 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,426 @@
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1"/>
+<title>Happy Raksha-Bandha by~ vikrantup72</title>
+    <meta name="google" value="notranslate">
+    <meta property="og:type" content="Make your own wonderful wish for you loved once" />
+    <meta property="og:title" content="Sent Raksha Bandhan Greetings to Your sister by vikrantup72" />
+    <meta property="og:url" content="https://TechRipon.com/>
+    <meta property="og:description" content="CLICK HERE" />
+    <meta property="og:site_name" content="Create Raksha Bandhan Wishes" />
+    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+    <link href='https://fonts.googleapis.com/css?family=Sofia:&effect=neon' rel='stylesheet'>
+	  <link rel="icon" type="image/png" sizes="32x32" href="https://i.imgur.com/fcqTtzV.png">
+<!------- Google Analytic Code Here ------------>
+
+<style>
+    i {
+        color:#f06414;
+    }
+input[type=name], select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 4px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+.footerbtn {
+ 
+            display: block;
+            line-height: 15px;
+            position: fixed;
+            left:0px;
+            bottom:0px;
+            height:60px;
+            
+border-radius: 15px;
+  box-sizing: border-box;
+  padding: 5px;
+  background:#34af23;
+  color: #ffffff;
+  font-size: 18px;
+  text-align: center;
+  text-decoration: none;
+  width:95%;
+ margin-left:10px;
+            margin-right:30px;
+            box-shadow: 0 4px 12px 0 rgba(0, 0, 0, .3);
+            animation: footer infinite linear 1s;
+            -webkit-transform: translate3d(30%,0,0);
+            transform: translate3d(30%,0,0);
+            position: fixed;
+           
+}
+
+.footerbtn :active {
+            box-shadow: none
+        }
+
+        @-webkit-keyframes footer {
+            from {
+                -webkit-transform: rotateZ(0)
+            }
+            25% {
+                -webkit-transform: rotateZ(1.5deg)
+            }
+            50% {
+                -webkit-transform: rotateZ(0deg)
+            }
+            75% {
+                -webkit-transform: rotateZ(-1.5deg)
+            }
+            to {
+                -webkit-transform: rotateZ(0)
+            }}
+
+@-webkit-keyframes jello {  from, 11.1%, to {    transform: none;  }
+  22.2% {    transform: skewX(-12.5deg) skewY(-12.5deg);  }
+  33.3% {    transform: skewX(6.25deg) skewY(6.25deg);  }
+  44.4% {    transform: skewX(-3.125deg) skewY(-3.125deg);  }
+  55.5% {    transform: skewX(1.5625deg) skewY(1.5625deg);  }
+  66.6% {    transform: skewX(-0.78125deg) skewY(-0.78125deg);  }
+  77.7% {    transform: skewX(0.390625deg) skewY(0.390625deg);  }
+  88.8% {    transform: skewX(-0.1953125deg) skewY(-0.1953125deg);  }}
+.jello {  -webkit-animation: jello 3s infinite;  transform-origin: center; -webkit-animation-delay:6s}
+@-webkit-keyframes hue {
+  from {    -webkit-filter: hue-rotate(0deg);  }
+  to {    -webkit-filter: hue-rotate(-360deg);  }}
+    .m1{position:fixed;left:1%; width:auto;height:100%;top:1%;color:#000;}
+    .m2{position:fixed;right:1%; width:auto;height:100%;top:1%;color:#000;}
+.bubbles {
+font-family: arial;
+}
+
+.bubbles hi {
+font-family: 'Luckiest Guy', cursive;
+color: black;
+}
+
+hi {
+ font-size:2.5em;
+ user-select:none;
+}
+
+hi span { display:inline-block; animation:float .2s ease-in-out infinite; }
+ @keyframes float {
+  0%,100%{ transform:none; }
+  33%{ transform:translateY(-1px) rotate(-2deg); }
+  66%{ transform:translateY(1px) rotate(2deg); }
+}
+body:hover span { animation:bounce .6s; }
+@keyframes bounce {
+  0%,100%{ transform:translate(0); }
+  25%{ transform:rotateX(20deg) translateY(2px) rotate(-3deg); }
+  50%{ transform:translateY(-20px) rotate(3deg) scale(1.1);  }
+}
+
+@import url(http://fonts.googleapis.com/css?family=Concert+One);
+
+h1 {
+  animation:glow 10s ease-in-out infinite;
+}
+
+
+
+* { box-sizing:border-box; }
+
+figure {
+  animation:wobble 5s ease-in-out infinite;
+  transform-origin:center center;
+  transform-style:preserve-3d;
+}
+
+@keyframes wobble {
+  0%,100%{ transform:rotate3d(1,1,0,40deg); }
+  25%{ transform:rotate3d(-1,1,0,40deg); }
+  50%{ transform:rotate3d(-1,-1,0,40deg); }
+  75%{ transform:rotate3d(1,-1,0,40deg); }
+}
+
+h1 {
+  display:block;
+  width:90%;
+  line-height:1.5;
+  font:900 35px 'Concert One', sans-serif;
+  position:absolute;
+  color:#fff;
+}
+
+@keyframes glow {
+  0%,100%{ text-shadow:0 0 30px red; }
+  25%{ text-shadow:0 0 30px orange; }
+  50%{ text-shadow:0 0 30px forestgreen; }
+  75%{ text-shadow:0 0 30px cyan; }
+}
+
+h1:nth-child(2){ transform:translateZ(5px); }
+h1:nth-child(3){ transform:translateZ(10px);}
+h1:nth-child(4){ transform:translateZ(15px); }
+h1:nth-child(5){ transform:translateZ(20px); }
+h1:nth-child(6){ transform:translateZ(25px); }
+h1:nth-child(7){ transform:translateZ(30p.rock{animation:infinite 1s rock}
+@keyframes rock {  0% {    transform: rotate(-1deg);  }
+  50% {    transform: rotate(2deg);  }
+  100% {    transform: rotate(-1deg);  }}x); }
+h1:nth-child(8){ transform:translateZ(35px); }
+h1:nth-child(9){ transform:translateZ(40px); }
+h1:nth-child(10){ transform:translateZ(45px); }
+
+
+
+.rock{animation:infinite 1s rock}
+@keyframes rock {  0% {    transform: rotate(-1deg);  }
+  50% {    transform: rotate(2deg);  }
+  100% {    transform: rotate(-1deg);  }}
+
+.fuck{animation:infinite 1s fuck}
+@keyframes rock {  0% {    transform: rotate(-2deg);  }
+  50% {    transform: rotate(2deg);  }
+  100% {    transform: rotate(-2deg);  }}
+
+
+h2 {
+   font-size: 38px;
+    text-align: center;
+    color:#008000;
+    
+    animation: rock infinite 1s;
+    font-family: 'Indie Flower', cursive;
+    letter-spacing: 2px;
+}
+
+h3 {
+    font-size: 48px;
+    text-align: center;
+    padding:1px;
+    margin:1px;
+    color: #f06414;
+   
+    animation: fuck infinite 1s;
+    font-family: 'Indie Flower', cursive;
+    letter-spacing: 2px;
+}
+	
+	*{box-sizing: border-box;}
+body {font-family: Verdana, sans-serif;}
+.mySlides {display: none;}
+img {vertical-align: middle;}
+
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/11 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active {
+  background-color: #717171;
+}
+
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 1.5s;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@-webkit-keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .text {font-size: 11px}
+	
+	
+	
+	
+}
+</style>
+
+</head>
+<body class="bg" id="rakhi" style="background: linear-gradient(to bottom right, #f6ff00, #e1e8e7, #00ffe9);
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;">
+
+<?php include 'raks.php';?>
+
+<!------- This Website was Created by vikrantup72 ------------>
+
+<center>
+<!------- AdSense Code Goes Here Top Header Ads 350x50p ------------>
+
+</center>
+<center><span class="hny-txt" style="font-size: 42px;">
+<img src="dd.png" width="35px"><b>
+<span style="color: red;">WELCOME</span> 
+<img src="dd.png" width="35px"></b>
+</span></center>
+<center>  
+  
+    
+<figure>
+    
+	<h1 style="text-transform: uppercase;"><script type="text/javascript" language="Javascript">
+               
+                 var a=prompt("Please Enter Your Name\n\n👇👇👇👇👇");
+                 
+                 document.write(a);
+                 </script> </h1>
+                
+	<h1 style="text-transform: uppercase;"><script type="text/javascript" language="Javascript">
+                    
+                    document.write(a);
+                      </script> </h1>
+	<h1 style="text-transform: uppercase;"><script type="text/javascript" language="Javascript">
+                   
+                    document.write(a);
+                      </script> </h1>
+	<h1 style="text-transform: uppercase;"><script type="text/javascript" language="Javascript">
+                    
+                    document.write(a);
+                      </script></h1>
+    <h1 style="text-transform: uppercase;"><script type="text/javascript" language="Javascript">
+                   
+                    document.write(a);
+                      </script> </h1>
+    <h1 style="text-transform: uppercase;"><script type="text/javascript" language="Javascript">
+                   
+                    document.write(a);
+                      </script> </h1>
+    <h1 style="text-transform: uppercase;"><script type="text/javascript" language="Javascript">
+                    
+                    document.write(a);
+                      </script></h1>
+
+</figure>
+ </center>
+<div class="vi" style="text-align: center;">
+
+<img src="Snow.gif" alt="long.jpg" style="width: 100%; height:100px;"><br><br><br>
+<center>
+<img src="advance.png" width="60%" height="10%"style="animation: tada 4s infinite"><br><br>
+<img src="happy.gif" width="80%" height="120px"style="animation: pulse 1.5s infinite"><br><br>
+</center>
+<center>
+<!------- AdSense Code Goes Here <Responsive ads> ------------>
+
+</center>
+<img src="dd.png" height="60px" width="60px"/>
+<img src="dd.png" height="60px" width="65px"/>
+<img src="dd.png" height="60px" width="65px"/>
+<img src="dd.png" height="60px" width="65px"/>
+<img src="dd.png" height="60px" width="65px"/>
+<center>
+<span class="font-effect-neon" style="font-size: 15pt; font-family: 'Sofia';">
+<span style="font-size: 28px;">Do You Know What is Rakhi?</span><br />
+र = 🌹रक्षा करना बहन की 🌹<br/>
+क्षा=🌹क्षमा करना बहन को 🌹<br/>
+बं = 🌹 बंधन से मुक्त करना बहन को 🌹<br/>
+ध = 🌹ध्यान रखना बहन का 🌹<br/>
+न = 🌹नही भूलना बहन को 🌹<br/>
+</span></p>
+<img src="bhaibhn.png"  class="swing2"  height="300px" width="260px"/>
+</center>
+<center>
+<p class="hny-txt">
+ <h3> <span style="font-size: 30px;"><b>We Love Each Other</span> <span style="color: #008000;"></span></h3></p></center>
+</center>
+
+<center>
+<div class="busi"><h4><p style="text-transform: uppercase;font-size: 30px;color:#ffffff;"> <script type="text/javascript" language="Javascript">                                       
+ document.write( "by - " +a);
+                      </script><br><br></p></h4></center>
+<center>
+<!------- AdSense Code Goes Here (Link Ads) ------------>
+
+</center>
+
+</div>
+        </div>
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Aug 22, 2021 00:00:00").getTime();
+
+// Update the count down every 01 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "<i> Days,</i> " + hours + " <i>Hrs,</i> "
+  + minutes + "<i> Min,</i> " + seconds + "<i> Sec</i> ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "Started";
+  }
+}, 1000);
+
+</script>       
+            </div>
+        </div>
+ </body>
+ <center>
+   <a class="footerbtn" href="whatsapp://send?text= 🇮🇳  *Touch This Blue line Enter Your name and See Magic*
+👉 https://happyrakshabandhan.netlify.app"><img width="25px" height="25px" src="https://i.imgur.com/fcqTtzV.png"/>  <b class="hny-txt" style="font-size: 26px;">Share on Whatsapp ~vikrantup72</b>  <img width="25px" height="25px" src="https://i.imgur.com/fcqTtzV.png"/></a></center>
+ 
+</script>
+
+</html>        
---
+++ raks.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Audio</title>
+</head>
+<body>
+<audio src="bhaibhn.mp3" id="my_audio" loop="loop"></audio>
+  <script type="text/javascript">
+    window.onload=function(){
+      document.getElementById("my_audio").play();
+    }
+  </script>
+</body>
+</html>
